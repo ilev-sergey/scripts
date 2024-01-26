a2bffdedd0ec242b8269c9b863a6c065533c0d1e
Sergey Ilev <ilev.sv@phystech.edu>
2024-01-26 21:33:06 +0300

    add function to calculate domain share in pfm map

--- pfm/process.py
+++ pfm/process.py
@@ -2,6 +2,8 @@ import logging
 from pathlib import Path
 
 import numpy as np
+from matplotlib import pyplot as plt
+from numpy.typing import NDArray
 
 from pfm.fitScanPFM import fitScanPFM
 from pfm.plotter import plot_amplitude, plot_params, plot_phase, plot_piezo
@@ -16,6 +18,29 @@ def process_all_data(data_path: Path, results_path: Path, functions):
         results = fitScanPFM(**data)
         for function in functions:
             function(results, path)
+
+
+def get_domains_distribution(results_path: Path):
+    def calculate_domains(phase: NDArray[np.float64]):  # TODO: move to another file
+        max = np.quantile(phase, 0.95)
+        min = np.quantile(phase, 0.05)
+        diff = max - min
+        if diff < np.pi / 2:
+            return 0, phase.mean()  # TODO: or 1
+        threshold = (max + min) / 2
+        small_phase = np.count_nonzero(phase <= threshold)
+        share = 1 - small_phase / phase.size
+
+        return share, threshold
+
+    pathlist = results_path.glob("**/phase.npy")
+    shares = []
+    for file in pathlist:
+        phase = np.load(file)
+        share, _threshold = calculate_domains(phase)
+        shares.append(share)
+
+    return np.array(shares)
 
 
 def save_results(results, path: Path):  # TODO: move to another file
@@ -42,4 +67,11 @@ def is_processed(results_path):  # TODO: pass datafile, not results path
 
 if __name__ == "__main__":
     # TODO: make that both results or data from files can be passed into main processing functions
-    print(is_processed("results/#7752_2/data_2023_12_06__04_56_51 +2.6"))
+    for path in Path().glob("results/#7755_2/"):
+        shares = get_domains_distribution(path)
+        shares = np.delete(shares, 2)
+        cycles = [0, 0.5, 1.5, 3.5, 5.5, 10.5, 20.5]
+        plt.plot(cycles, shares, label=path.stem)
+        plt.legend()
+
+        plt.savefig("domains")
