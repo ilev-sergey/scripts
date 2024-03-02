0be0a60a8d7387493b2b12fd413bd7694ae15598
Sergey Ilev <ilev.sv@phystech.edu>
2024-03-02 11:47:57 +0300

    update main.py with new functionality

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import logging
 from pathlib import Path
 
-from pfm.plotter import plot_amplitude, plot_params, plot_phase, plot_piezo
+from pfm.plot import (
+    plot_amp_phase_log,
+    plot_amplitude,
+    plot_params,
+    plot_phase,
+    plot_piezo,
+)
 from pfm.process import process_all_data, save_results
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
-# TODO: UI to choose data path and results path
 
 data_path = Path("data")
 results_path = Path("results")
@@ -14,6 +19,7 @@ functions_to_apply = [
     plot_piezo,
     plot_phase,
     plot_amplitude,
+    plot_amp_phase_log,
     save_results,
 ]
-process_all_data(data_path, results_path, functions_to_apply)  # type: ignore
+process_all_data(data_path, results_path, functions_to_apply, cache=True)  # type: ignore
